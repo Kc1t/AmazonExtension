2094b3cb6187fd21f162f6310d340c1ff19f7a2d
Kauã Miguel <98243777+Kc1t@users.noreply.github.com>
2023-02-03 18:42:20 -0300

    start

--- Index.js
+++ Index.js
@@ -1,1 +1,15 @@
 'use strict'
+
+async function Exten(){
+
+    const Name = document.querySelector('.product-title-word-break.a-size-large').innerText;
+   
+    const Brand = document.querySelector('.po-brand.a-spacing-small > .a-span9 > .po-break-word.a-size-base').innerText;
+
+    const Cat = document.querySelector('.nav-b.nav-a > .nav-a-content').innerText
+
+    const Weight = document.querySelector('.po-item_weight.a-spacing-small > .a-span9 > .po-break-word.a-size-base').innerText;
+    
+
+    //Nome, Marca, Categoria, Dimensões, EAN, Link das imagens
+}
