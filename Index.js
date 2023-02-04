2bd39b09c2fca21c482fdc81e18d284ab21c7525
Kauã Miguel <98243777+Kc1t@users.noreply.github.com>
2023-02-04 15:14:16 -0300

    0.0.5 Copy func

--- Index.js
+++ Index.js
@@ -1,41 +1,80 @@
 'use strict';
 
 async function Exten(){
-
-    const Name = document.querySelector('.product-title-word-break.a-size-large')?.innerText || 'Não Informado';
-   
-    const Brand = document.querySelector('.po-brand.a-spacing-small > .a-span9 > .po-break-word.a-size-base')?.innerText || 'Não Informado';
-
-    const Cat = document.querySelector('.nav-b.nav-a > .nav-a-content')?.innerText || 'Não Informado';
-
-    const Weight = document.querySelector('.po-item_weight.a-spacing-small > .a-span9 > .po-break-word.a-size-base')?.innerText || 'Não Informado';
     
-    const BaseInner = document.querySelector('.priceToPay.reinventPricePriceToPayMargin.aok-align-center.a-price > span');
+    //Nome, Marca, Categoria, Dimensões, EAN, Link das imagens
 
-    const Dim = document.querySelector('.po-item_dimensions.a-spacing-small > .a-span9 > .po-break-word.a-size-base')?.innerText || 'Não Informado'
+    const
+    Name = document.querySelector('.product-title-word-break.a-size-large')?.innerText || 'Não Informado',
+    Brand = document.querySelector('.po-brand.a-spacing-small > .a-span9 > .po-break-word.a-size-base')?.innerText || 'Não Informado',
+    Cat = document.querySelector('.nav-b.nav-a > .nav-a-content')?.innerText || 'Não Informado',
+    Weight = document.querySelector('.po-item_weight.a-spacing-small > .a-span9 > .po-break-word.a-size-base')?.innerText || 'Não Informado',
+    BaseInner = document.querySelector('.priceToPay.reinventPricePriceToPayMargin.aok-align-center.a-price > span'),
+    Dim = document.querySelector('.po-item_dimensions.a-spacing-small > .a-span9 > .po-break-word.a-size-base')?.innerText || 'Não Informado',
+    EAN = document.querySelector('tr:nth-of-type(16) > .prodDetAttrValue.a-size-base')?.innerText || 'Não Informado';
 
-    const EAN = document.querySelector('tr:nth-of-type(16) > .prodDetAttrValue.a-size-base')?.innerText || 'Não Informado';
-
-    setTimeout(()=>{
+    // setTimeout(()=>{
         BaseInner.insertAdjacentHTML(
             "beforebegin",
             `
             <ul class="AmazContainer">
             
-            <li>Nome: <span>${Name}</span></li>
-            <li>Marca: <span>${Brand}</span></li>
-            <li>Categoria: <span>${Cat}</span></li>
-            <li>Peso: <span>${Weight}</span></li>
-            <li>Dimensões C x L x A: <span>${Dim}</span></li>
-            <li>EAN: <span>${EAN}</span></li>
+            <li>Nome: <span id="NameCopy">${Name}</span></li>
+            <li>Marca: <span id="BrandCopy">${Brand}</span></li>
+            <li>Categoria: <span id="CatCopy">${Cat}</span></li>
+            <li>Peso: <span id="WeightCopy">${Weight}</span></li>
+            <li>Dimensões C x L x A: <span id="DimCopy">${Dim}</span></li>
+            <li>EAN: <span id="EANCopy">${EAN}</span></li>
             </ul>
 
             `
         )
-        
-    },0)
 
-    //Nome, Marca, Categoria, Dimensões, EAN, Link das imagens
+    
+        //Copiar Texto da ${}, irei encurtar isso
+
+        const nameCopy = document.querySelector("#NameCopy");
+        nameCopy.addEventListener("click", function() {
+            copyText(nameCopy.textContent);
+          });
+
+        const brandCopy = document.querySelector("#BrandCopy")
+        brandCopy.addEventListener("click", function() {
+            copyText(brandCopy.textContent);
+          });
+
+        const catCopy = document.querySelector("#CatCopy")
+        catCopy.addEventListener("click", function() {
+            copyText(catCopy.textContent);
+          });
+
+        const weightCopy = document.querySelector("#WeightCopy")
+        weightCopy.addEventListener("click", function() {
+            copyText(weightCopy.textContent);
+          });
+          
+        const dimCopy = document.querySelector("#DimCopy")
+        dimCopy.addEventListener("click", function() {
+            copyText(dimCopy.textContent);
+          });
+
+        const eanCopy = document.querySelector("#EANCopy")
+        eanCopy.addEventListener("click", function() {
+            copyText(eanCopy.textContent);
+          });
+        
+        function copyText(textToCopy) {
+            const textarea = document.createElement("textarea");
+            textarea.value = textToCopy;
+            document.body.appendChild(textarea);
+            textarea.select();
+            document.execCommand("copy");
+            textarea.remove();
+            console.log("COPIADO")
+          }
+
+    // },1000)
+
 }
 
 Exten()
