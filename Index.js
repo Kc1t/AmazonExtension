255fc6dbc63bbe963fbe7acb2605bfa9e6c10c8c
Kauã Miguel <98243777+Kc1t@users.noreply.github.com>
2023-02-04 08:35:38 -0300

    lineBreak

--- Index.js
+++ Index.js
@@ -2,17 +2,19 @@
 
 async function Exten(){
 
-    const Name = document.querySelector('.product-title-word-break.a-size-large')?.innerText || '0';
+    const Name = document.querySelector('.product-title-word-break.a-size-large')?.innerText || 'Não Informado';
    
-    const Brand = document.querySelector('.po-brand.a-spacing-small > .a-span9 > .po-break-word.a-size-base')?.innerText || '0';
+    const Brand = document.querySelector('.po-brand.a-spacing-small > .a-span9 > .po-break-word.a-size-base')?.innerText || 'Não Informado';
 
-    const Cat = document.querySelector('.nav-b.nav-a > .nav-a-content')?.innerText || '0';
+    const Cat = document.querySelector('.nav-b.nav-a > .nav-a-content')?.innerText || 'Não Informado';
 
-    const Weight = document.querySelector('.po-item_weight.a-spacing-small > .a-span9 > .po-break-word.a-size-base')?.innerText || '0';
+    const Weight = document.querySelector('.po-item_weight.a-spacing-small > .a-span9 > .po-break-word.a-size-base')?.innerText || 'Não Informado';
     
-    const BaseInner = document.querySelector('.priceToPay.reinventPricePriceToPayMargin.aok-align-center.a-price > span')
+    const BaseInner = document.querySelector('.priceToPay.reinventPricePriceToPayMargin.aok-align-center.a-price > span');
 
-    const EAN = document.querySelector('tr:nth-of-type(16) > .prodDetAttrValue.a-size-base')?.innerText || '0'
+    const Dim = document.querySelector('.po-item_dimensions.a-spacing-small > .a-span9 > .po-break-word.a-size-base')?.innerText || 'Não Informado'
+
+    const EAN = document.querySelector('tr:nth-of-type(16) > .prodDetAttrValue.a-size-base')?.innerText || 'Não Informado';
 
     setTimeout(()=>{
         BaseInner.insertAdjacentHTML(
@@ -24,8 +26,8 @@ async function Exten(){
             <li>Marca: <span>${Brand}</span></li>
             <li>Categoria: <span>${Cat}</span></li>
             <li>Peso: <span>${Weight}</span></li>
+            <li>Dimensões C x L x A: <span>${Dim}</span></li>
             <li>EAN: <span>${EAN}</span></li>
-            <li>Link Imagem: <span></span></li>
             </ul>
 
             `
@@ -37,3 +39,14 @@ async function Exten(){
 }
 
 Exten()
+
+
+// finding content by inner
+
+var headings = document.evaluate("//tr[contains(., 'EAN')]", document, null, XPathResult.ANY_TYPE, null );
+var thisHeading = headings.iterateNext();
+
+console.log(thisHeading); // Prints the html element in console
+console.log(thisHeading.textContent); // prints the text content in console
+
+thisHeading.innerHTML += "<br />Modified contents";  
